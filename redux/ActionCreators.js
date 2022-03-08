afbfaaa7fd2262aa5e89ba1fa6d71d7e45067354
Mykel Dicus <mykeldicus@gmail.com>
2022-03-07 20:45:14 -0800

    Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -31,6 +31,25 @@ export const addComments = comments => ({
     payload: comments
 });
 
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    newComment.date = new Date().toISOString();
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+};
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
 export const fetchCampsites = () => dispatch => {
 
     dispatch(campsitesLoading());
@@ -48,7 +67,8 @@ export const fetchCampsites = () => dispatch => {
             error => {
                 const errMess = new Error(error.message);
                 throw errMess;
-            })
+            }
+        )
         .then(response => response.json())
         .then(campsites => dispatch(addCampsites(campsites)))
         .catch(error => dispatch(campsitesFailed(error.message)));
@@ -85,7 +105,8 @@ export const fetchPromotions = () => dispatch => {
             error => {
                 const errMess = new Error(error.message);
                 throw errMess;
-            })
+            }
+        )
         .then(response => response.json())
         .then(promotions => dispatch(addPromotions(promotions)))
         .catch(error => dispatch(promotionsFailed(error.message)));
@@ -122,7 +143,8 @@ export const fetchPartners = () => dispatch => {
             error => {
                 const errMess = new Error(error.message);
                 throw errMess;
-            })
+            }
+        )
         .then(response => response.json())
         .then(partners => dispatch(addPartners(partners)))
         .catch(error => dispatch(partnersFailed(error.message)));
@@ -142,8 +164,8 @@ export const addPartners = partners => ({
     payload: partners
 });
 
-export const PostFavorite = campsiteId => dispatch => {
-    setTimeout (() => {
+export const postFavorite = campsiteId => dispatch => {
+    setTimeout(() => {
         dispatch(addFavorite(campsiteId));
     }, 2000);
 };
